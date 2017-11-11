f0045d94ad73d4b1e4e864fb85c9e9e8a913e0da
dh-dev-prog <Damien@MacBook-Pro-de-Damien.local>
2017-11-11 16:42:11 +0900

    Add new movie to data and modify tests

--- scenarios.js
+++ scenarios.js
@@ -1,70 +1,63 @@
-describe('app', function(){
+describe('app', () => {
 
-  beforeEach(function(){
-    browser.get('index.html');
-  });
+  beforeEach(() => browser.get('index.html'));
 
-  describe('appRoot', function(){
-    it('should display each view properly', function(){
+  describe('appRoot', () => {
+
+    it('should display each view properly', () => {
       var links = element.all(by.css('a'));
       var preUrl = 'http://localhost:8080/index.html#!';
-      links.first().click().then(function(){
-        expect(browser.getCurrentUrl()).toBe(`${preUrl}/watchlist`);
-      });
-      links.get(1).click().then(function(){
-        expect(browser.getCurrentUrl()).toBe(`${preUrl}/home`);
-      });
-      links.get(2).click().then(function(){
-        expect(browser.getCurrentUrl()).toBe(`${preUrl}/watched`);
-      });
+      links.first().click().then(() => expect(browser.getCurrentUrl()).toBe(`${preUrl}/watchlist`));
+      links.get(1).click().then(() => expect(browser.getCurrentUrl()).toBe(`${preUrl}/home`));
+      links.get(2).click().then(() => expect(browser.getCurrentUrl()).toBe(`${preUrl}/watched`));
     });
+
   });
 
-  describe('movieList', function(){
+  describe('movieList', () => {
 
-    it('should filter the movieList', function(){
+    it('should filter the movieList', () => {
       var movieList = element.all(by.repeater('movie in $ctrl.list'));
       var query = element(by.model('$ctrl.livesearch'));
 
-      expect(movieList.count()).toBe(3);
+      expect(movieList.count()).toBe(4);
 
       query.sendKeys('vikings');
       expect(movieList.count()).toBe(1);
 
       query.clear();
       query.sendKeys('r');
-      expect(movieList.count()).toBe(2);
+      expect(movieList.count()).toBe(3);
     });
 
-    it('should order the list accordingly to the dropdown-menu', function(){
+    it('should order the list accordingly to the dropdown-menu', () => {
       var queryField = element(by.model('$ctrl.livesearch'));
       var orderSelect = element(by.model('$ctrl.order'));
       var nameOption = orderSelect.element(by.css('option[value="name"]'));
       var movieNameColumn = element.all(by.repeater('movie in $ctrl.list').column('movie.name'));
 
-      function getNames() {
-        return movieNameColumn.map(function(elem){
-          return elem.getText();
-        });
-      }
+      let getNames = () => movieNameColumn.map(elem => elem.getText());
+
       queryField.sendKeys('r');
       expect(getNames()).toEqual([
         'Mr. Robot - Season 3',
-        'Blade Runner 2049'
+        'Blade Runner 2049',
+        'The Shape Of Water',
       ]);
       nameOption.click();
       expect(getNames()).toEqual([
         'Blade Runner 2049',
-        'Mr. Robot - Season 3'
+        'Mr. Robot - Season 3',
+        'The Shape Of Water'
       ]);
     });
 
-    it('should display a checkmark when button "add" is clicked', function(){
-      element.all(by.repeater('movie in $ctrl.list')).then(function(movies) {
-         movies.forEach(function(movie){
+    it('should display a checkmark when button "add" is clicked', () => {
+      element.all(by.repeater('movie in $ctrl.list')).then(movies => {
+         movies.forEach(movie => {
           var button = movie.element(by.className('movie-list-add'));
           expect(button.getText()).toEqual('Add');
-          button.click().then(function(){
+          button.click().then(() => {
            var newEl = movie.element(by.className('movie-list-checked'));
            expect(newEl.getText()).toEqual('✓');
           })
